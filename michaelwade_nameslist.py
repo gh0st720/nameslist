45243f84f627a9d1132b01c2fba8cabbaf08ec2e
Michael Wade <55414281+gh0st720@users.noreply.github.com>
2019-10-21 14:46:45 -0400

    Add files via upload

---
+++ michaelwade_nameslist.py
@@ -0,0 +1,30 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+== RESTART: C:\Users\ghost\OneDrive\Documents\GitHub\pythonlistpractice.py ==
+enter first name:1
+enter first name:2
+enter first name:3
+enter first name:4
+enter first name:5
+enter first name:6
+enter first name:7
+enter first name:8
+enter first name:9
+enter first name:20
+enter search name2
+2 was found
+enter search name3
+3 was found
+enter search name4
+4 was found
+enter search name5
+5 was found
+enter search name6
+6 was found
+enter search name7
+7 was found
+enter search name8
+8 was found
+enter search nameEnd
+>>> 
