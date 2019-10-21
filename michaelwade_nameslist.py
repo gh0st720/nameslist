d702148f6c2d1b9ada72abeb16e64617aafcac7c
Michael Wade <55414281+gh0st720@users.noreply.github.com>
2019-10-21 14:48:10 -0400

    Add files via upload

--- michaelwade_nameslist.py
+++ michaelwade_nameslist.py
@@ -1,30 +1,24 @@
-Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-== RESTART: C:\Users\ghost\OneDrive\Documents\GitHub\pythonlistpractice.py ==
-enter first name:1
-enter first name:2
-enter first name:3
-enter first name:4
-enter first name:5
-enter first name:6
-enter first name:7
-enter first name:8
-enter first name:9
-enter first name:20
-enter search name2
-2 was found
-enter search name3
-3 was found
-enter search name4
-4 was found
-enter search name5
-5 was found
-enter search name6
-6 was found
-enter search name7
-7 was found
-enter search name8
-8 was found
-enter search nameEnd
->>> 
+#List creating and searching
+end = False 
+names = []
+for x in range(0,10):
+    aname = input("enter first name:")
+    names.append(aname)
+while not end:
+    search = input("enter search name")
+
+    if search == ("End"):
+        end = True 
+    elif search in names:
+        print(search, "was found")
+    
+    elif search not in names:
+        print(search, "was not found")
+    
+
+
+
+
+
+
+               
